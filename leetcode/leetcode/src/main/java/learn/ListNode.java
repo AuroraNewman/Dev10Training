4588b611fa48a9bf34d44bcae088fe42e655dcb7
Aurora Newman <auroranewmansoftware@gmail.com>
2025-04-04 12:09:02 -0400

    created listnode generating helper method; solved mergesortedlinkedlists with new listnode method

--- leetcode/leetcode/src/main/java/learn/ListNode.java
+++ leetcode/leetcode/src/main/java/learn/ListNode.java
@@ -6,4 +6,28 @@ public class ListNode {
     ListNode() {}
     ListNode(int val) { this.val = val; }
     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    public static ListNode generateListNodeFromArray(int[] nums){
+        if (nums.length == 0){
+            return null;
+        } else if (nums.length == 1){
+            return new ListNode(nums[0]);
+        }
+        ListNode listHead = new ListNode();
+        ListNode currentNode = new ListNode();
+        for (int i = 0; i < nums.length; i++){
+            currentNode.val=nums[i];
+            if (i < nums.length - 1){
+                ListNode next = new ListNode(nums[i+1]);
+                if (i == 0){
+                    listHead = currentNode;
+                }
+                currentNode.next=next;
+                currentNode=currentNode.next;
+            } else {
+                currentNode.next=null;
+            }
+
+        }
+        return listHead;
+    }
 }
--- leetcode/leetcode/src/main/java/learn/MergeSortedLinkedLists.java
+++ leetcode/leetcode/src/main/java/learn/MergeSortedLinkedLists.java
@@ -2,42 +2,28 @@ package learn;
 //from https://leetcode.com/problems/merge-two-sorted-lists/description/
 public class MergeSortedLinkedLists {
 
-
         public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+            ListNode listHead = new ListNode();
+            ListNode currentNode = listHead;
 
-         if (list1 == null){
-             return list2;
-         } else if (list2 == null){
-             return list1;
-         }
-         ListNode resultHead = new ListNode();
-            ListNode result = new ListNode();
-            if (list1.val < list2.val) {
-                result = list1;
-                resultHead = list1;
-                list1 = list1.next;
-            } else {
-                result = list2;
-                resultHead = list2;
-                list2 = list2.next;
+            while (list1 != null && list2 != null){
+                if (list1.val < list2.val){
+                    currentNode.next=list1;
+                    list1=list1.next;
+                } else {
+                    currentNode.next=list2;
+                    list2=list2.next;
+                }
+                currentNode = currentNode.next;
             }
-         while (list1 != null && list2 != null) {
-             if (list1.val < list2.val) {
-                 result.next = list1;
-                 list1 = list1.next;
-             } else {
-                 result.next = list2;
-                 list2 = list2.next;
-             }
-             result = result.next;
-         }
-         if (list1 == null && list2 != null){
-             result.next = list2;
-         } else if (list2 == null && list1 != null){
-             result.next = list1;
-         }
 
-            return resultHead;
+            if (list1 == null){
+                currentNode.next = list2;
+            } else {
+                currentNode.next = list1;
+            }
+
+            return listHead.next;
         }
     }
 
