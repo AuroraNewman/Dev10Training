eaee23010976b6d2a7aa1cb1b68acd17c4b1df15
Aurora Newman <auroranewmansoftware@gmail.com>
2025-04-14 21:35:47 -0400

    remove element in place

--- leetcode/leetcode/src/main/java/learn/RemoveElement.java
+++ leetcode/leetcode/src/main/java/learn/RemoveElement.java
@@ -8,21 +8,22 @@ https://leetcode.com/problems/remove-element/
  */
 public class RemoveElement {
     public int removeElement(int[] nums, int val) {
-        if (nums.length == 0){
-            return 0;
-        }
-        ArrayList<Integer> numsList = new ArrayList<>();
         int notVal = 0;
-        for (int i = 0; i < nums.length; i++){
-            if (nums[i] == val){
+        if (nums.length == 0){
+            return notVal;
+        }
+        ArrayList<Integer> listInts = new ArrayList<>();
+
+        for (int number : nums){
+            if (number != val){
+                listInts.add(number);
                 notVal++;
-            } else {
-                numsList.add(nums[i]);
             }
         }
-        for (int i = 0; i < numsList.size(); i++){
-            nums[i] = numsList.get(i);
+        for (int i = 0; i < notVal; i++){
+            nums[i] = listInts.get(i);
         }
+
         return notVal;
     }
 }
